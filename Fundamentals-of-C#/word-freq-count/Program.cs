7f448a99480d2dbdf4df6503e49350f8b9d5a7f6
betiiy-haile <betelhemhaile74@gmail.com>
2025-03-04 22:31:58 +0300

    added word count challenge

---
+++ Fundamentals-of-C#/word-freq-count/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Please, Enter a word: ");
+string word = Console.ReadLine() ?? string.Empty;
+
+word = word.ToLower();
+
+Dictionary<char, int> charCount = new Dictionary<char, int>();
+foreach (char c in word)
+{
+    if (charCount.ContainsKey(c))
+    {
+        charCount[c]++;
+    }
+    else
+    {
+        charCount.Add(c, 1);
+    }
+}
+
+foreach (var item in charCount)
+{
+    Console.WriteLine($"Character: {item.Key}, Count: {item.Value}");
+}
