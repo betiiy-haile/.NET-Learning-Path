5f79130b39ed000e25ba533995d017e926fe6527
betiiy-haile <betelhemhaile74@gmail.com>
2025-03-04 23:04:58 +0300

    added palindrome check

---
+++ Fundamentals-of-C#/Palindrome-check/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Please the word you want to check for palindrome: ");
+string word = Console.ReadLine() ?? string.Empty;
+
+string normalizedWord = NormalizeString(word);
+if(isPalindrome(normalizedWord))
+{
+    Console.WriteLine($"{word} is a palindrome");
+}
+else
+{
+    Console.WriteLine($"{word} is not a palindrome");
+}
+
+static bool isPalindrome(string word)
+{
+    int left = 0;
+    int right = word.Length - 1;
+
+    while (left < right)
+    {
+        if (word[left] != word[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+static string NormalizeString(string input)
+{
+    string result = "";
+
+    foreach(char c in input)
+    {
+        if (char.IsLetter(c))
+        {
+            result += c;
+        }
+
+    }
+    return result;
+}
